7bc6982296ed469a7c976cf48e388537e0e44830
alex.fedotov <alex.fedotov@saymon.info>
2024-06-20 16:55:55 +0300

    format_on_save redefined

--- init.lua
+++ init.lua
@@ -1,3 +1,19 @@
 return {
   colorscheme = "tokyonight",
+  polish = function()
+    -- Get access to the lsp module and its settings
+    local lsp_utils = require('astronvim.utils.lsp')
+
+    -- Redifine formatting settings
+    lsp_utils.formatting = {
+      format_on_save = {
+        enabled = true, -- true or false
+        ignore_filetypes = {
+          "yaml",
+          "dockerfile",
+        },
+      },
+      disabled = lsp_utils.formatting.disabled or {},
+    }
+  end,
 }
